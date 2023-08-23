14d8c8115fb53f75690896a97ac794232d05b7a1
Roman Shrestha <roman@romans-mbp.myfiosgateway.com>
2023-08-23 16:48:13 -0400

    Added Customers page with backend setup

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js"
 import ProductStat from "../models/ProductStat.js"
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -21,3 +22,12 @@ export const getProducts = async (req, res) => {
       res.status(404).json({ message: error.message });
     }
   };
+
+export const getCustomers = async(req, res) => {
+  try {
+    const customers = await User.find({role: "user"}).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
