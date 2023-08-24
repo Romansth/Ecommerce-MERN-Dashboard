e94bf3dc0ed2e0e469a51dc5663db2a88b478fcb
Roman Shrestha <roman@romans-mbp.myfiosgateway.com>
2023-08-24 00:11:36 -0400

    Added Transactions page with server side pagination

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js"
 import ProductStat from "../models/ProductStat.js"
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js"
 
 export const getProducts = async (req, res) => {
     try {
@@ -27,6 +28,44 @@ export const getCustomers = async(req, res) => {
   try {
     const customers = await User.find({role: "user"}).select("-password");
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const getTransactions = async(req, res) => {
+  try {
+    //sort should look like this: {"field", "userId", "sort", "desc"}
+    const { page = 1, pageSize=20, sort= null, search = ""} = req.query;
+
+    //formatted sort should look like {userId: -1}
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+      }; 
+      return sortFormatted; 
+    }
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [
+        {cost: { $regex: new RegExp(search, "i")}},
+        {userId: { $regex: new RegExp(search, "i")}}
+      ],
+    })
+    .sort(sortFormatted)
+    .skip(page * pageSize)
+    .limit(pageSize);
+
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i"}
+    });
+
+    res.status(200).json({
+      transactions,
+      total,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
